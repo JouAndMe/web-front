4311e5c9a3b7ba7c16600fd5e09c0b9749afd037
jinrung2 <fnzmdpfak661@gmail.com>
2023-08-04 02:05:31 +0900

    [FIX] #1 - error fix

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,13 @@
 import React from "react";
 const Header = () => {
+    
     return(
-        <>
-        헤더
-        </>
+        <div>
+            <div className="head">
+                <div className="headMenu" alt="logo" src="img/logo.jpg"></div>
+                <div className="headMenu" alt="kb_logo" src="img/kb_logo.jpg"></div>
+            </div>
+        </div>
     )
 }
 export default Header
---
+++ src/pages/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../components/css/component.css'
+import Nav from "../components/Nav";
+import Header from "../components/Header";
+
+
+const Main = () => {
+    return (
+        <>
+            <Header />
+            <Nav />
+        </>
+    )
+}
+export default Main
