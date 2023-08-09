a0876a93252229de728019db36ce0864f7bd1759
sejeonghwang <tpwjddl987@naver.com>
2023-08-10 01:47:59 +0900

    [UI] UI 수정작업

--- src/components/Dash.js
+++ src/components/Dash.js
@@ -1,19 +1,24 @@
 import React from "react";
 import "../styles/Dash.css";
+
 const Dash = () => {
     return(
         <div className="dashboard">
             <div className="chatting">
                 <>채팅하는 곳 ㅎ,ㅎ</>
+                <div className="input-container">
+                    <input
+                        type="text"
+                        className="chat-input"
+                    />
+                    <button
+                        type="button"
+                        className="btn-send"
+                    >
+                        Send
+                    </button>
+                </div>
             </div>
-            <input
-                type="text"
-                className="chat-input"
-            />
-            <button
-                type="button"
-                className="btn-send"
-            />
         </div>      
     )
 }
